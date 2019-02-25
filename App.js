f62a806008ab9158fbabf60c88ad58722816b068
wolfjobs <wolfjobs@163.com>
2019-02-25 19:23:02 +0800

    init project, add modalShow

---
+++ App.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {Platform,TouchableOpacity, StyleSheet, Text, View} from 'react-native';
+import ModalShow from "./src/components/ModalShow";
+import GetRef from "./src/common/GetRef";
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View
+        style={{flex: 1}}
+      >
+        <ModalShow
+          ref={ref => {
+            GetRef.setTopLevelRef(ref)
+          }}
+        />
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>To get started, edit App.js</Text>
+          <Text style={styles.instructions}>{instructions}</Text>
+          <TouchableOpacity
+            onPress={()=>{
+              GetRef.show('显示Modal显示Modal')
+            }}
+          >
+            <Text style={styles.welcome}>显示Modal</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
